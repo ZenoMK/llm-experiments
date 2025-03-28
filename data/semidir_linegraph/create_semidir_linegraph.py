a781826735c9b8a76745558125c97dfb2c77f126
Zeno Kujawa <30478937+ZenoMK@users.noreply.github.com>
2025-03-28 08:50:04 -0400

    semidir bigger window

--- data/semidir_linegraph/create_semidir_linegraph.py
+++ data/semidir_linegraph/create_semidir_linegraph.py
@@ -16,7 +16,7 @@ def generate_undirected_linegraph(num_nodes):
     # Add edges to form a line (path) graph
     for i in range(num_nodes - 1):
         G.add_edge(i, i + 1)
-    for i in range(48,51):
+    for i in range(48,53):
         G.add_edge(i+1,i)
 
     print(G.edges)
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     data = numpy.zeros([num_nodes, num_nodes])
     for source_node in range(48):
         cnt = 0  # to avoid some target not appear in training dataset
-        for target_node in range(52, num_nodes):
+        for target_node in range(54, num_nodes):
             if source_node == target_node:
                 continue
             elif random.random() < chance_in_train:
