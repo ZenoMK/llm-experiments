5f6593dee65fd13891a20f1788dfb4f4df45bedb
Zeno Kujawa <30478937+ZenoMK@users.noreply.github.com>
2025-04-04 23:06:30 -0400

    docker!

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ python create_circle.py --num_nodes 95
 
 python prepare_minigpt_path.py --num_nodes 95 --num_of_paths 20 --graph_type circle --problem path
 
+python train.py --num_nodes 100 --problem path --dataset circle
+
+python test_simple.py --num_nodes 100 --num_of_paths 20 --problem path --graph_type circle
+
 
