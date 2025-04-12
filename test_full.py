9ad70bc56ee62d181145e8ea7cac572c7b25fbdb
Zeno Kujawa <30478937+ZenoMK@users.noreply.github.com>
2025-04-11 21:18:33 -0400

    start adapting testing and training with full embeddings

--- test_full.py
+++ test_full.py
@@ -16,18 +16,20 @@ import torch
 from datetime import datetime
 
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument('--ckpt_iter', type=int, default=10000)
-parser.add_argument('--graph_type', type=str, default='simple_graph')
-parser.add_argument('--config', type=str, default='1_1_120')
-parser.add_argument('--temperature', type=float, default=1)
-parser.add_argument('--device', type=str, default='cpu')
-parser.add_argument('--num_nodes', type=int, default=100)
-parser.add_argument('--num_of_paths', type=int, default=20)
-parser.add_argument("--problem", type=str, default="path", help="Which algorithmic problem (path/cut)")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ckpt_iter', type=int, default=10000)
+    parser.add_argument('--graph_type', type=str, default='simple_graph')
+    parser.add_argument('--config', type=str, default='1_1_120')
+    parser.add_argument('--temperature', type=float, default=1)
+    parser.add_argument('--device', type=str, default='cpu')
+    parser.add_argument('--num_nodes', type=int, default=100)
+    parser.add_argument('--num_of_paths', type=int, default=20)
+    parser.add_argument("--problem", type=str, default="path", help="Which algorithmic problem (path/cut)")
+    parser.add_argument('--embedding_config', type=str, default='', help='Optional suffix for embedding config (e.g., _identity_nopos)')
 
-args = parser.parse_args()
+args = parse_args()
 dataset = args.graph_type
 ckpt_iter = args.ckpt_iter
 problem = args.problem
