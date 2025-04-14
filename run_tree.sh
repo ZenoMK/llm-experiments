c14cad66e3ead75a524f1390dee09cbabfb6ae9a
Zeno Kujawa <30478937+ZenoMK@users.noreply.github.com>
2025-04-14 10:33:07 -0400

    start adapting testing and training with full embeddings

--- run_tree.sh
+++ run_tree.sh
@@ -11,6 +11,8 @@
 
 module load gcc/11.1.0
 
+module load anaconda3/2022.05
+
 conda init bash
 
 conda activate pytorch_env
@@ -19,7 +21,7 @@ python generate_tree_path.py --num_nodes 100
 
 python prepare_minigpt_path.py --num_nodes 100 --problem tree --graph_type tree
 
-python train_full.py --num_nodes 100 --problem tree --num_of_paths 20 --dataset tree --n_head 1 --n_layer 1 --use_identity_embeddings
+python train_full.py --num_nodes 100 --problem tree --num_of_paths 20 --dataset tree --n_head 1 --n_layer 1 --use_identity_embeddings --max_iter 1000
 
-python test_full.py --num_nodes 100 --num_of_paths 20 --graph_type tree --problem tree
+python test_full.py --num_nodes 100 --num_of_paths 20 --graph_type tree --problem tree --ckpt_iter 1000
 
