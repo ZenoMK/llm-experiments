ff49dd6bf507ccbbf59afb41d17b8a72935b313b
Zeno Kujawa <30478937+ZenoMK@users.noreply.github.com>
2025-04-15 16:47:57 -0400

    check copying

--- create_list_copy.py
+++ create_list_copy.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    folder_name = os.path.join(os.path.dirname(__file__), f'{args.num_nodes}_list')
+    folder_name = os.path.join(os.path.dirname(__file__), f'data/list/{args.num_nodes}_list_copy')
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
